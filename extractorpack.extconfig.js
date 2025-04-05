9138424d518449f011bc203d0ee029b1027e4346
iTsMaaT <matoulimar1@gmail.com>
2025-04-05 12:28:04 -0400

    feat: add extractor pack configuration for Soundgasm

---
+++ extractorpack.extconfig.js
@@ -0,0 +1,3 @@
+export const EXTRACTOR_PACK_CLASS = "SoundgasmExtractor";
+export const EXTRACTOR_PACK_EXT_OPTIONS = "SoundgasmExtractorOptions";
+export const EXTRACTOR_PACK_TYPES = "/dist/index.d.ts";
